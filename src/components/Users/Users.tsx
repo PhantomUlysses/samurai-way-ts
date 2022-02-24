6a69b3e1468237f8b28c8b7c6eb25a1eae7c4444
Nikita Stasiulevich <nikitastas1694@gmail.com>
2022-02-25 00:31:58 +0300

    remove side effect from Users component, 52 lesson

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -6,23 +6,27 @@ import userPhoto from '../../assets/images/user.png';
 
 
 const Users = (props: UsersPropsType) => {
-    if (props.usersPage.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items);
-        })
+    let getUsers = () => {
+        if (props.usersPage.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items);
+            })
+        }
     }
 
     return <div>
+        <button onClick={getUsers}>Get Users</button>
         {
             props.usersPage.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} alt="avatar" />
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}
+                             alt="avatar"/>
                     </div>
                     <div>
-                        { u.followed
+                        {u.followed
                             ? <button onClick={() => props.unfollow(u.id)}>Unfollow</button>
-                            : <button onClick={() => props.follow(u.id)}>Follow</button> }
+                            : <button onClick={() => props.follow(u.id)}>Follow</button>}
                     </div>
                 </span>
                 <span>
